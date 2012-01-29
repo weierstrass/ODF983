9a5d864813f5bc1afc8cbf0f60e9c0e8198a6889
Weierstrass <andreas@bulling.se>
2012-01-29 19:08:05 +0100

    added precalc of w / A(i, i)

--- sor_ds.c
+++ sor_ds.c
@@ -5,26 +5,33 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #include "common.h"
 #include "cblas.h"
 
-void sor_ds(double** A, double* b, double* x, int n, double w, double tol){
+void sor_ds(double* A, double* b, double* x, int n, double w, double tol){
   int i;
-  double err, x_old;
+  double err, x_old, *wa;
+  wa = (double*) malloc(n * sizeof(double));
+  for(i = 0; i < n; i++) wa[i] = w / *(A + i + n * i);
 
   while(1){
     err = 0;
 
     for(i = 0; i < n; i++){
       x_old = x[i];
-      x[i] = x[i] + (b[i] - cblas_ddot(n, A[i], 1, x, 1)) * w / A[i][i];
-      err += fabs(x_old - x[i]);
+      x[i] = x[i] + (b[i] - cblas_ddot(n, (A + n * i), 1, x, 1)) * wa[i];
+      
+      x_old = fabs(x_old - x[i]); 
+      err +=  x_old * x_old;
     }
     //convergence check
-    if(err / n < tol) break;
+    if(sqrt(err) < tol) break;
   }
+  
+  free(wa);
 
 }
 
