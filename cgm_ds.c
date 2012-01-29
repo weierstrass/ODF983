fd273875aac2cf19b4963ddcffc9d0fe07793734
Weierstrass <andreas@bulling.se>
2012-01-29 18:11:38 +0100

    polishing

--- cgm_ds.c
+++ cgm_ds.c
@@ -6,37 +6,48 @@
  */
 
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "cblas.h"
 
 void cgm_ds(double* A, double* b, double* x, int n, double tol){
-  int k;
-  double alpha, beta, *r, r_old_dot, *p, *tmp;
+  double alpha, beta, r_old_dot, *r, *p, *tmp;
   r = (double*) malloc(n * sizeof(double));
   p = (double*) malloc(n * sizeof(double));
   tmp = (double*) malloc(n * sizeof(double));
   
-  cblas_dcopy(n, b, 1, r, 1); // b -> r TODO fixa, eg b - Ax0
-  cblas_dcopy(n, b, 1, p, 1); // b -> p
+  //set initial vals on p and r
+  cblas_dcopy(n, b, 1, r, 1); // b - Ax_0 -> r
+  cblas_dsymv(CblasRowMajor, CblasUpper, n, -1.0, A, n, x, 1, 1.0, r, 1);
+  cblas_dcopy(n, r, 1, p, 1); // b - Ax_0 -> p
 
-  for(k = 0; ; k++){
-    //printf("k: %d, x[0] = %.15f\n", k, x[0]);
-    cblas_dgemv(CblasRowMajor, CblasNoTrans, n, n, 1, A, n, p, 1, 0, tmp, 1);
-    //cblas_dsymv(CblasRowMajor, CblasUpper, n, 1, A, n, p, 1, 0, tmp, 1);
+  while(1){
+    //alpha_k = dot(r_k, r_k) / dot(p_k, p_k)_A
+    cblas_dsymv(CblasRowMajor, CblasUpper, n, 1, A, n, p, 1, 0, tmp, 1);
     alpha = cblas_ddot(n, r, 1, r, 1) / cblas_ddot(n, p, 1, tmp, 1);
-    //printf("alpha = %.15f\n", alpha);
+    
+    //x_k+1 = x_k + alpha_k*p_k 
     cblas_daxpy(n, alpha, p, 1, x, 1);
-
+    
     r_old_dot = cblas_ddot(n, r, 1, r, 1);
+
+    //r_k+1 = r_k - alpha_k*A*p_k
     cblas_daxpy(n, -alpha, tmp, 1, r, 1);
 
+    //break if residual is small enough
     if(cblas_dnrm2(n, r, 1) < tol) break;
 
+    //beta_k = dot(r_k+1, r_k+1) / dot(r_k, r_k)
     beta = cblas_ddot(n, r, 1, r, 1) / r_old_dot;
+
+    //p_k+1 = r_k+1 + beta_k*p_k
     cblas_dscal(n, beta, p, 1);
     cblas_daxpy(n, 1, r, 1, p, 1);
   }
-}
 
+  free(r);
+  free(p);
+  free(tmp);
+
+}
