3de5626bda4b31062cf8fdafccfb5bb5116162dc
Weierstrass <andreas@bulling.se>
2012-01-30 22:23:50 +0100

    fixed typos

--- odf983.h
+++ odf983.h
@@ -6,3 +6,4 @@
 void sor_ds(double* A, double* b, double* x, int n, double w, double tol);
 //void ssor_dummy(double** A, double* b, double* x, int n, double w, double tol);
 void cgm_ds(double* A, double* b, double* x, int n, double tol);
+void grdm_ds(double* A, double* b, double* x, int n, double tol);
