bc1179d29b7a44a1fbeab21ebdeec2c87a7b9b2a
Weierstrass <andreas@bulling.se>
2012-01-31 12:28:47 +0100

    cleanup

--- linsys/grdm_ds.c
+++ linsys/grdm_ds.c
@@ -16,11 +16,6 @@ void grdm_ds(double* A, double* b, double* x, int n, double tol){
   d = (double*) malloc(n * sizeof(double));
   tmp = (double*) malloc(n * sizeof(double));
   
-  //set initial vals on p and r
-  //cblas_dcopy(n, b, 1, r, 1); // b - Ax_0 -> r
-  //cblas_dsymv(CblasRowMajor, CblasUpper, n, -1.0, A, n, x, 1, 1.0, r, 1);
-  //cblas_dcopy(n, r, 1, p, 1); // b - Ax_0 -> p
-
   while(1){
     //printf("x[0] = %f\n", x[0]);
     //d_k = A*x_k - b
@@ -39,27 +34,6 @@ void grdm_ds(double* A, double* b, double* x, int n, double tol){
 
     //convergence check
     if(cblas_dnrm2(n, tmp, 1) < tol) break;
-/*     //alpha_k = dot(r_k, r_k) / dot(p_k, p_k)_A */
-/*     cblas_dsymv(CblasRowMajor, CblasUpper, n, 1, A, n, p, 1, 0, tmp, 1); */
-/*     alpha = cblas_ddot(n, r, 1, r, 1) / cblas_ddot(n, p, 1, tmp, 1); */
-    
-/*     //x_k+1 = x_k + alpha_k*p_k  */
-/*     cblas_daxpy(n, alpha, p, 1, x, 1); */
-    
-/*     r_old_dot = cblas_ddot(n, r, 1, r, 1); */
-
-/*     //r_k+1 = r_k - alpha_k*A*p_k */
-/*     cblas_daxpy(n, -alpha, tmp, 1, r, 1); */
-
-/*     //break if residual is small enough */
-/*     if(cblas_dnrm2(n, r, 1) < tol) break; */
-
-/*     //beta_k = dot(r_k+1, r_k+1) / dot(r_k, r_k) */
-/*     beta = cblas_ddot(n, r, 1, r, 1) / r_old_dot; */
-
-/*     //p_k+1 = r_k+1 + beta_k*p_k */
-/*     cblas_dscal(n, beta, p, 1); */
-/*     cblas_daxpy(n, 1, r, 1, p, 1); */
   }
 
   free(d);
